d8116d68775849ac004643bbeabc90afcf29e2d9
EngMaiBelal <EngMaiBelal>
2025-01-20 03:09:58 +0200

    Seeder-Factory

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,16 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('user_name');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('image');
+            $table->boolean('status')->default(1);
+            $table->string('street');
+            $table->string('city');
+            $table->string('country');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = ['Mobile Category', 'Sports Category', 'Fashion Category'];
+        $date = fake()->date('Y-m-d h:m:s');
+
+        foreach($data as $category){
+            Category::create([
+                "name" => $item,
+                "slug" => str::slug($item),
+                "status" => rand(0,1),
+                "created_at" => $date,
+                "updated_at" => $date,
+            ]);
+        };
+    }
+}
